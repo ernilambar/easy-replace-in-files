330b73bc5204162e5547aa5e48080cdde024406d
Nilambar Sharma <nilambar@outlook.com>
2022-09-03 12:41:06 +0545

    feat: standard

--- src/app.js
+++ src/app.js
@@ -1,85 +1,85 @@
-import fs from 'fs';
-import path from 'path';
-import chalk from 'chalk';
-import unixify from 'unixify';
-import replaceInFile from 'replace-in-file';
+import fs from 'fs'
+import path from 'path'
+import chalk from 'chalk'
+import unixify from 'unixify'
+import replaceInFile from 'replace-in-file'
 
-import { isEmptyObject, getParamValue, replacePlaceholders } from './utils.js';
+import { isEmptyObject, getParamValue, replacePlaceholders } from './utils.js'
 
-const cwd = unixify( process.cwd() );
+const cwd = unixify(process.cwd())
 
 const easyRelaceInFiles = () => {
-	let configFile = '';
+  let configFile = ''
 
-	if ( fs.existsSync( path.resolve( cwd, 'easy-replace-in-files.json' ) ) ) {
-		configFile = 'easy-replace-in-files.json';
-	} else {
-		console.log( `Config file not found! Please create ${ chalk.yellow( 'easy-replace-in-files.json' ) } file.` );
-		process.exit();
-	}
+  if (fs.existsSync(path.resolve(cwd, 'easy-replace-in-files.json'))) {
+    configFile = 'easy-replace-in-files.json'
+  } else {
+    console.log(`Config file not found! Please create ${chalk.yellow('easy-replace-in-files.json')} file.`)
+    process.exit()
+  }
 
-	const configFilePath = path.join( cwd, configFile );
+  const configFilePath = path.join(cwd, configFile)
 
-	let configData;
+  let configData
 
-	try {
-		configData = JSON.parse( fs.readFileSync( configFilePath ) );
-	} catch ( err ) {
-		console.error( err );
-	}
+  try {
+    configData = JSON.parse(fs.readFileSync(configFilePath))
+  } catch (err) {
+    console.error(err)
+  }
 
-	const list = ( 'easyReplaceInFiles' in configData ) ? configData.easyReplaceInFiles : {};
+  const list = ('easyReplaceInFiles' in configData) ? configData.easyReplaceInFiles : {}
 
-	if ( isEmptyObject( list ) ) {
-		console.log( `${ chalk.yellow( 'easyReplaceInFiles' ) } key not found in config file.` );
-		process.exit();
-	}
+  if (isEmptyObject(list)) {
+    console.log(`${chalk.yellow('easyReplaceInFiles')} key not found in config file.`)
+    process.exit()
+  }
 
-	list.forEach( function( item ) {
-		const defaults = { files: '', from: '', to: '', type: 'string' };
+  list.forEach(function (item) {
+    const defaults = { files: '', from: '', to: '', type: 'string' }
 
-		item = { ...defaults, ...item };
+    item = { ...defaults, ...item }
 
-		if ( ! Array.isArray( item.files ) && '' === item.files ) {
-			console.log( `${ chalk.cyan( 'WARN' ) }: ${ chalk.yellow( 'files' ) } key missing in the rule. Skipping.` );
-			return;
-		}
+    if (!Array.isArray(item.files) && item.files === '') {
+      console.log(`${chalk.cyan('WARN')}: ${chalk.yellow('files')} key missing in the rule. Skipping.`)
+      return
+    }
 
-		let filesValue = ( Array.isArray( item.files ) ) ? item.files : [ item.files ];
+    let filesValue = (Array.isArray(item.files)) ? item.files : [item.files]
 
-		filesValue = filesValue.map( ( k ) => replacePlaceholders( k ) );
+    filesValue = filesValue.map((k) => replacePlaceholders(k))
 
-		if ( ! Array.isArray( item.from ) && '' === item.from ) {
-			console.log( `${ chalk.cyan( 'WARN' ) }: ${ chalk.yellow( 'from' ) } key missing in the rule. Skipping.` );
-			return;
-		}
+    if (!Array.isArray(item.from) && item.from === '') {
+      console.log(`${chalk.cyan('WARN')}: ${chalk.yellow('from')} key missing in the rule. Skipping.`)
+      return
+    }
 
-		let fromValue = ( Array.isArray( item.from ) ) ? item.from : [ item.from ];
+    let fromValue = (Array.isArray(item.from)) ? item.from : [item.from]
 
-		fromValue = fromValue.map( ( element ) => getParamValue( element, item.type ) );
+    fromValue = fromValue.map((element) => getParamValue(element, item.type))
 
-		let toValue = '';
+    let toValue = ''
 
-		if ( Array.isArray( item.to ) ) {
-			toValue = item.to.map( ( element ) => replacePlaceholders( element ) );
-		} else {
-			toValue = replacePlaceholders( item.to );
-		}
+    if (Array.isArray(item.to)) {
+      toValue = item.to.map((element) => replacePlaceholders(element))
+    } else {
+      toValue = replacePlaceholders(item.to)
+    }
 
-		const options = {
-			files: filesValue,
-			from: fromValue,
-			to: toValue,
-		};
+    const options = {
+      files: filesValue,
+      from: fromValue,
+      to: toValue
+    }
 
-		try {
-			replaceInFile.sync( options );
-		} catch ( error ) {
-			console.error( 'Error occurred:', error );
-		}
-	} );
+    try {
+      replaceInFile.sync(options)
+    } catch (error) {
+      console.error('Error occurred:', error)
+    }
+  })
 
-	console.log( chalk.green( 'Replacing complete.' ) );
-};
+  console.log(chalk.green('Replacing complete.'))
+}
 
-export { easyRelaceInFiles };
+export { easyRelaceInFiles }
